0b86179e61818088de61feb59241aea797345b0f
Jay SyKo <jay@jaysyko.com>
2018-02-14 17:06:23 -0500

    Added snack detection

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import numpy as np
 import imutils
 import cv2
 
+def get_square_center(c):
+	M = cv2.moments(c)
+	try:
+		cX = int(M["m10"] / M["m00"])
+		cY = int(M["m01"] / M["m00"])
+		return cX, cY
+	except Exception as e:
+		print(e)
+		return 0, 0
+
+def collinear(p1, p2):
+	return (p1[0] == p2[0]) or (p1[1] == p2[1])
+
 while True:
 	screen_image = ImageGrab.grab(bbox=(13, 80, 950, 1030))
 	screen = np.array(screen_image)
@@ -14,15 +27,13 @@ while True:
 
 	cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-	for c in cnts:
-		M = cv2.moments(c)
-		cX = int(M["m10"] / M["m00"])
-		cY = int(M["m01"] / M["m00"])
-		c = c.astype("int")
-		cv2.drawContours(screen, [c], -1, (0, 255, 0), 2)
-	
+	if len(cnts) == 2:
+		p1, p2 = get_square_center(cnts[0]), get_square_center(cnts[1])
+		if collinear(p1, p2):
+			cv2.line(screen, p1, p2, (0, 255, 0), 10)
+
 	cv2.imshow("Vue", screen)
-	
+
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 		break
--- snake_detection.py
+++ snake_detection.py
@@ -10,11 +10,17 @@ def detect_snake_head(image):
 	# head mask
 	green = np.uint8([156, 204, 101])
 	head_mask = cv2.inRange(image, green, green)
-	return cv2.bitwise_and(image, image, mask=head_mask)
+	snack = np.uint8([233, 30, 99])
+	snack_mask = cv2.inRange(image, snack, snack)
+	mask = cv2.bitwise_or(head_mask, snack_mask)
+	return cv2.bitwise_and(image, image, mask=mask)
 
-	cnts = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	cnts = cvts[0] if imutils.is_cv2() else cnts[1]
-	print()
+def find_contours(image):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
+
+	cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 	for c in cnts:
 		M = cv2.moments(c)
 		cX = int((M["m10"] / M["m00"]) * ratio)
