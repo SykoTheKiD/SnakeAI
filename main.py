12c46bd4c38acd6caf60817e0e2077458a01eb57
Jay SyKo <jay@jaysyko.com>
2018-02-16 15:28:23 -0500

    Added area and distance to body

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from random import randint
 from game import SnakeGame
 from PIL import ImageGrab
 import numpy as np
+import logging
 import imutils
 import cv2
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def main():
 	game = SnakeGame()
@@ -19,9 +23,10 @@ def main():
 		thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
 		cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-		if len(cnts) == 2:
-			p1, p2 = sd.get_square_center(cnts[0]), sd.get_square_center(cnts[1])
-			dist_body = sd.point_distance(p1, p2)
+		if len(cnts) == 1:
+			M = cv2.moments(cnts[0])
+			snake_body_area = M['m00']
+			body_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
 		snake_mask = sd.get_snake_head_mask(screen)
 		candy_mask = sd.get_snake_candy_mask(screen)
@@ -41,7 +46,13 @@ def main():
 			dist_right = sd.point_distance(p1, (cols, p1[1]))
 			dist_top = sd.point_distance(p1, (p1[0], rows))
 			dist_bottom = sd.point_distance(p1, (p1[0], 0))
-	
+
+		dist_head_tail = 0
+		try:
+			dist_head_tail = sd.point_distance(p1, body_center)
+		except UnboundLocalError as e:
+			logger.error("No snake body found")
+
 		# Get distance between snake head and candy
 		full_mask = sd.combine_mask(snake_mask, candy_mask)
 		screen_full = cv2.bitwise_and(screen, screen, mask=full_mask)
@@ -53,7 +64,19 @@ def main():
 			p1, p2 = sd.get_square_center(cnts[0]), sd.get_square_center(cnts[1])
 			if sd.collinear(p1, p2):
 				dist_candy = sd.point_distance(p1, p2)
+		try:
+			input_vector = np.array([
+				dist_top, 
+				dist_bottom, 
+				dist_left, 
+				dist_right, 
+				dist_body, 
+				dist_candy
+				])
+		except UnboundLocalError as e:
+			logger.error("Game not ready")
 
+		cv2.imshow("Vue", screen_snake_body)
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
 			break
