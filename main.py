23a847f602026e88e7271aea243e992e98ec2b7f
Jay SyKo <jay@jaysyko.com>
2018-02-17 19:02:28 -0500

    Code Cleanup

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ def main():
 		screen = np.array(screen_image)
 
 		# Get distance between snake head and snake body
-		body_mask = sd.get_snake_body_mask(screen)
-		screen_snake_body = cv2.bitwise_and(screen, screen, mask=body_mask)
+		screen_snake_body = sd.get_part(screen, sd.SNAKE_BODY).screen
 		gray = cv2.cvtColor(screen_snake_body, cv2.COLOR_BGR2GRAY)
 		thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
 		cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -28,9 +27,10 @@ def main():
 			snake_body_area = M['m00']
 			body_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		snake_mask = sd.get_snake_head_mask(screen)
-		candy_mask = sd.get_snake_candy_mask(screen)
-		screen_snake = cv2.bitwise_and(screen, screen, mask=snake_mask)
+		snake_obj = sd.get_part(screen, sd.SNAKE_HEAD)
+		snake_mask = snake_obj.mask
+		candy_mask = sd.get_part(screen, sd.CANDY).mask
+		screen_snake = snake_obj.screen
 		
 		# Get distance between snake head and wall
 		rows = screen.shape[0]
@@ -40,6 +40,7 @@ def main():
 		thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
 		cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+		dist_top, dist_left, dist_right, dist_bottom = -1, -1, -1, -1
 		if len(cnts) == 1:
 			p1 = sd.get_square_center(cnts[0])
 			dist_left = sd.point_distance(p1, (0, p1[1]))
@@ -47,36 +48,35 @@ def main():
 			dist_top = sd.point_distance(p1, (p1[0], rows))
 			dist_bottom = sd.point_distance(p1, (p1[0], 0))
 
-		dist_head_tail = 0
+		dist_head_tail = -1
 		try:
 			dist_head_tail = sd.point_distance(p1, body_center)
 		except UnboundLocalError as e:
 			logger.error("No snake body found")
 
 		# Get distance between snake head and candy
-		full_mask = sd.combine_mask(snake_mask, candy_mask)
-		screen_full = cv2.bitwise_and(screen, screen, mask=full_mask)
+		full = sd.combine(screen, snake_mask, candy_mask)
+		screen_full = full.screen
 		gray = cv2.cvtColor(screen_full, cv2.COLOR_BGR2GRAY)
 		thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
 		cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+		dist_candy = -1
 		if len(cnts) == 2:
 			p1, p2 = sd.get_square_center(cnts[0]), sd.get_square_center(cnts[1])
 			if sd.collinear(p1, p2):
 				dist_candy = sd.point_distance(p1, p2)
-		try:
-			input_vector = np.array([
-				dist_top, 
-				dist_bottom, 
-				dist_left, 
-				dist_right, 
-				dist_body, 
-				dist_candy
-				])
-		except UnboundLocalError as e:
-			logger.error("Game not ready")
 
-		cv2.imshow("Vue", screen_snake_body)
+		input_vector = np.array([
+			dist_top, 
+			dist_bottom, 
+			dist_left, 
+			dist_right, 
+			dist_head_tail, 
+			dist_candy
+			])
+
+		cv2.imshow("Vue", screen_snake)
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
 			break
--- snake_detection.py
+++ snake_detection.py
@@ -3,6 +3,14 @@ import numpy as np
 import logging
 import cv2
 
+SNAKE_BODY = "snake_body"
+SNAKE_HEAD = "snake_head"
+CANDY = "candy"
+
+COLOURS = {SNAKE_BODY: np.uint8([255, 255, 255]), 
+		   SNAKE_HEAD: np.uint8([156, 204, 101]), 
+		   CANDY: np.uint8([233, 30, 99])}
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -10,21 +18,20 @@ def contour_approximation(contours):
 	perimeter = cv2.arcLength(c, True)
 	approx = cv2.approxPolyDP(c, 0.04 * perimeter, True)
 
-def get_snake_body_mask(image):
-	white = np.uint8([255, 255, 255])
-	return cv2.inRange(image, white, white)
+def get_part(image, part):
+	try:
+		colour = COLOURS[part]
+		mask = cv2.inRange(image, colour, colour)
+		screen = cv2.bitwise_and(image, image, mask=mask)
+		return Screen(mask, screen)
+	except KeyError as e:
+		logger.error("Invalid Part")
 
-def get_snake_head_mask(image):
-	# head mask
-	green = np.uint8([156, 204, 101])
-	return cv2.inRange(image, green, green)
-
-def get_snake_candy_mask(image):
-	snack = np.uint8([233, 30, 99])
-	return cv2.inRange(image, snack, snack)
 	
-def combine_mask(mask1, mask2):
-	return cv2.bitwise_or(mask1, mask2)
+def combine(image, mask1, mask2):
+	mask = cv2.bitwise_or(mask1, mask2)
+	screen = cv2.bitwise_and(image, image, mask=mask)
+	return Screen(mask, screen)
 
 def find_contours(image):
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -56,3 +63,8 @@ def point_distance(p1, p2):
 	x_delta = p2[0] - p1[0]
 	y_delta = p2[1] - p1[1]
 	return sqrt((x_delta**2 + y_delta**2))
+
+class Screen:
+	def __init__(self, mask, screen):
+		self.mask = mask
+		self.screen = screen
