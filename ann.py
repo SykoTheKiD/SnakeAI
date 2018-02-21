dfc63e666211af25d7874c0fa4e9af2c1ae591fe
Jay SyKo <jay@jaysyko.com>
2018-02-21 15:18:19 -0500

    refactored nn

--- ann.py
+++ ann.py
@@ -21,7 +21,7 @@ class NeuralNetwork:
         self.output_layer = {'weights': tf.Variable(tf.random_normal([num_nodes_hl3, self.num_classes])),
                              'biases': tf.Variable(tf.random_normal([self.num_classes]))}
         self.layers = [self.hl_1, self.hl_2, self.hl_3, self.output_layer]
-        x_input_placeholder = tf.placeholder(shape=[None, self.input_size], dtype=tf.float32)
+        self.x_input_placeholder = tf.placeholder(shape=[None, self.input_size], dtype=tf.float32)
         l1 = tf.add(tf.matmul(x_input_placeholder, self.hl_1['weights']), self.hl_1['biases'])
         l1 = tf.nn.relu(l1)
         l2 = tf.add(tf.matmul(l1, self.hl_2['weights']), self.hl_2['biases'])
@@ -33,7 +33,7 @@ class NeuralNetwork:
     def predict(self, input_vector):
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            layer_eval = self.output_layer_final.eval(feed_dict={x_input_placeholder: [input_vector]})
+            layer_eval = self.output_layer_final.eval(feed_dict={self.x_input_placeholder: [input_vector]})
             result = sess.run(tf.argmax(layer_eval, 1))
         return result[0]
 
